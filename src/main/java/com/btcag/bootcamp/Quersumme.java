c86575a07bad74dd5cda3ab8d10da5bbfa50cb01
LADIEKEN <Lara.Diekena@btc-ag.com>
2024-09-17 16:09:40 +0200

    Aufgabe3 1/2

---
+++ src/main/java/com/btcag/bootcamp/Quersumme.java
@@ -0,0 +1,19 @@
+package com.btcag.bootcamp;
+
+import java.util.Scanner;
+
+public class Quersumme {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long gZahl = sc.nextLong();
+        long quersumme = 0;
+
+        while (gZahl != 0) {
+            quersumme = quersumme + gZahl%10;
+            gZahl = gZahl/10;
+        }
+        System.out.println(quersumme);
+
+        String zKette = sc.nextLine();
+    }
+}
