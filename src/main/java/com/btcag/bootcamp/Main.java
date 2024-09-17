1a89651204bdd5ff5fcbd8d48cdcbd3456d312c4
LADIEKEN <Lara.Diekena@btc-ag.com>
2024-09-17 14:08:28 +0200

    Aufgaben

--- src/main/java/com/btcag/bootcamp/Main.java
+++ src/main/java/com/btcag/bootcamp/Main.java
@@ -1,8 +1,32 @@
 package com.btcag.bootcamp;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        //Berechnung nach Eingabe im Terminal
+        /*
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bitte Zahl zur Quadratzahlberehunung eingeben:");
+        long thenumber = sc.nextLong();
+        thenumber *= thenumber; //äquivalent zu thenumber = thenumber * thenumber
+        System.out.println("Ergebnis: " + thenumber);
+        */
+
+        //Berechnung von Dezimalzahl in Binärzahl mit Modulo 2
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bitte positive ganze Zahl zum Umrechnen eingeben: ");
+        int dezimal = sc.nextInt();
+        String res = "";
+
+        while (dezimal>0) {
+            int resModulo = dezimal % 2;
+            dezimal = dezimal/2;
+            res = resModulo + res;
+        }
+
+        System.out.print("Die eingegebene Zahl entspricht im Binärsystem den Wert: " + res);
+
     }
 }
