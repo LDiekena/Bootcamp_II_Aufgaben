7ed5c862f985257a6c5fb3c6fdf84c5f82444b56
LDiekena <Lara.Diekena@btc-ag.com>
2024-09-24 07:30:19 +0200

    Aufgaben Fibonacci

--- src/main/java/com/btcag/bootcamp/W01_Fibonacci.java
+++ src/main/java/com/btcag/bootcamp/W01_Fibonacci.java
@@ -1,7 +1,23 @@
 package com.btcag.bootcamp;
 
+import java.util.Scanner;
+
 public class W01_Fibonacci {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Geben Sie eine ganze Zahl ein, für welche sie die Fibonacci Zahlen haben möchten ein: ");
+        int eingabe = sc.nextInt();
+        int res = 0;
+        int res2 = 1;
+        int res3 = 0;
+
+        while (res <= eingabe) {
+            System.out.println(res);
+            res3 = res + res2;
+            res2 = res;
+            res = res3;
+
+        }
 
     }
 }
