c68c838579cb5d50deda46e209dc9cbf80ad47a9
LDiekena <Lara.Diekena@btc-ag.com>
2024-09-24 07:14:14 +0200

    Aufgaben ToUpperCase

--- src/main/java/com/btcag/bootcamp/W01_ToUpperCase.java
+++ src/main/java/com/btcag/bootcamp/W01_ToUpperCase.java
@@ -1,7 +1,23 @@
 package com.btcag.bootcamp;
 
+import java.util.Scanner;
+
 public class W01_ToUpperCase {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bitte gib einen kleingeschriebenen Satz ein, welcher in große Buchstaben umgewandelt werden" +
+                "soll:");
+        String text = sc.nextLine();
 
+        int zaehler = 0;
+        String res = "";
+
+        while (zaehler < text.length()) {
+            int low = text.charAt(zaehler);
+            char up = (char)(low-32);
+            res = res + up;
+            zaehler++;
+        }
+        System.out.print(res);
     }
 }
