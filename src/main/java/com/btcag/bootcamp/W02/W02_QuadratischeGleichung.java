f988697e44c0def3e54ab865e6c5bd360e502ce4
LDiekena <Lara.Diekena@btc-ag.com>
2024-09-25 13:14:00 +0200

    Aufgabe QuadratischeGleichung + Schaltjahre

--- src/main/java/com/btcag/bootcamp/W02/W02_QuadratischeGleichung.java
+++ src/main/java/com/btcag/bootcamp/W02/W02_QuadratischeGleichung.java
@@ -1,4 +1,26 @@
 package com.btcag.bootcamp.W02;
 
+import java.util.Scanner;
+
 public class W02_QuadratischeGleichung {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bitte gebe den ersten Parameter a der quadratischen Gleichung ein: ");
+        int a = sc.nextInt();
+        System.out.println("Bitte gebe den zweiten Parameter b der quadratischen Gleichung ein: ");
+        int b = sc.nextInt();
+        System.out.println("Bitte gebe den dritten Parameter c der quadratischen Gleichung ein: ");
+        int c = sc.nextInt();
+
+        if (a > 0 && b > 0 && c > 0) {
+            System.out.println("Bei der Formel f(x) = " + a + "x^2 + " + b + "x " + c + " gibt es insgesamt "
+            + "2 Lösungen");
+        } else if ((a > 0 || a == 0) && b > 0 && (c > 0 || c == 0)) {
+            System.out.println("Bei der Formel f(x) = " + a + "x^2 + " + b + "x "  + c + " gibt es insgesamt "
+                    + "1 Lösung");
+        } else {
+            System.out.println("Bei der Formel f(x) = " + a + "x^2 + " + b + "x " + c + " gibt es insgesamt " +
+                    "0 Lösungen");
+        }
+    }
 }
--- src/main/java/com/btcag/bootcamp/W02/W02_Schaltjahre.java
+++ src/main/java/com/btcag/bootcamp/W02/W02_Schaltjahre.java
@@ -1,4 +1,18 @@
 package com.btcag.bootcamp.W02;
 
+import java.util.Scanner;
+
 public class W02_Schaltjahre {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bitte geben Sie eine Jahreszahl an bei der Sie überprüfen möchten ob es sich um ein" +
+                " Schaltjahr handelt: ");
+        long eingabe = sc.nextLong();
+
+        if ((eingabe % 4 == 0) && (eingabe % 100 > 0 || eingabe % 400 == 0)) {
+            System.out.println("Das Jahr " + eingabe + " ist ein Schaltjahr.");
+        } else {
+            System.out.println("Das Jahr " + eingabe + " ist kein Schaltjahr.");
+        }
+    }
 }
