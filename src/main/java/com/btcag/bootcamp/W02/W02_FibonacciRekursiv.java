11dbb9fcfa855bd1371abd380a3549a2c55790cc
LDiekena <Lara.Diekena@btc-ag.com>
2024-09-25 14:56:41 +0200

    Aufgabe FibonacciRekursiv

--- src/main/java/com/btcag/bootcamp/W02/W02_FibonacciRekursiv.java
+++ src/main/java/com/btcag/bootcamp/W02/W02_FibonacciRekursiv.java
@@ -1,4 +1,26 @@
 package com.btcag.bootcamp.W02;
 
+import java.util.Scanner;
+
 public class W02_FibonacciRekursiv {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Geben Sie die Anzahl von Iterationen für die Sie die Fibonacci Zahlen haben möchten ein: ");
+        int eingabe = sc.nextInt();
+
+        System.out.println("Die Reihe an Fibonacci Zahlen mit " + eingabe + " Iteratoinen ist folgende: ");
+        for (int i = 0 ; i < eingabe ; i++) {
+            System.out.print(fibonacciRekursiv(i) + " ");
+        }
+    }
+
+    public static int fibonacciRekursiv(int eingabe) {
+        if (eingabe == 0) {
+            return 0;
+        } else if (eingabe == 1 || eingabe == 2) {
+            return 1;
+        } else {
+            return fibonacciRekursiv(eingabe - 2) + fibonacciRekursiv(eingabe - 1);
+        }
+    }
 }
